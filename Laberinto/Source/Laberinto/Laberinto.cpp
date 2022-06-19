f352541562c3dfd1b208dc37ee660754ba76d1e6
Dario-ESAT <artilesdi@esat-alumni.com>
2022-06-19 17:47:04 +0100

    final

--- Laberinto/Source/Laberinto/Laberinto.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Laberinto.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Laberinto, "Laberinto" );
- 
